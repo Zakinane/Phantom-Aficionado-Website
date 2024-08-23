10975d0ee6e1faf1577b95107a98952d06d6a87c
Zak <djabazaki@gmail.com>
2024-08-24 00:23:13 +0100

    Switched to TS!

--- js/script.js
+++ js/script.js
@@ -1,52 +1,48 @@
 ///////////////////////////////////////////////////////////THE PAGES
-let currentPage = 1;
-
+var currentPage = 1;
 function showPage(pageNumber) {
-  const container = document.querySelector(".container");
-  container.style.transform = `translateX(-${(pageNumber - 1) * 100}vw)`;
-  currentPage = pageNumber;
-  updateIcons();
-}
-
-function updateIcons() {
-  const icons = document.querySelectorAll(".icon");
-  icons.forEach((icon, index) => {
-    if (index === currentPage - 1) {
-      icon.classList.add("active");
-    } else {
-      icon.classList.remove("active");
+    var container = document.querySelector(".container");
+    if (container) {
+        container.style.transform = "translateX(-".concat((pageNumber - 1) * 100, "vw)");
+        currentPage = pageNumber;
     }
-  });
+    updateIcons();
 }
-
+function updateIcons() {
+    var icons = document.querySelectorAll(".icon");
+    icons.forEach(function (icon, index) {
+        if (index === currentPage - 1) {
+            icon.classList.add("active");
+        }
+        else {
+            icon.classList.remove("active");
+        }
+    });
+}
 document.addEventListener("touchstart", handleTouchStart, false);
 document.addEventListener("touchmove", handleTouchMove, false);
-
-let xDown = null;
-
+var xDown = null;
 function handleTouchStart(evt) {
-  const firstTouch = evt.touches[0];
-  xDown = firstTouch.clientX;
+    var firstTouch = evt.touches[0];
+    xDown = firstTouch.clientX;
 }
-
 function handleTouchMove(evt) {
-  if (!xDown) {
-    return;
-  }
-
-  let xUp = evt.touches[0].clientX;
-  let xDiff = xDown - xUp;
-
-  if (xDiff > 0) {
-    if (currentPage < 4) { //to increment for every page added "Phorum - Poll - Notifications - IM"
-      showPage(currentPage + 1);
+    if (!xDown) {
+        return;
     }
-  } else {
-    if (currentPage > 1) {
-      showPage(currentPage - 1);
+    var xUp = evt.touches[0].clientX;
+    var xDiff = xDown - xUp;
+    if (xDiff > 0) {
+        if (currentPage < 4) {
+            //to increment for every page added "Phorum - Poll - Notifications - IM"
+            showPage(currentPage + 1);
+        }
     }
-  }
-  xDown = null;
+    else {
+        if (currentPage > 1) {
+            showPage(currentPage - 1);
+        }
+    }
+    xDown = null;
 }
-
 ////////////////////////////////////////////////////////Poll of the week
---
+++ js/script.ts
@@ -0,0 +1,55 @@
+///////////////////////////////////////////////////////////THE PAGES
+let currentPage = 1;
+
+function showPage(pageNumber: number) {
+  const container: HTMLElement | null = document.querySelector(".container");
+  if (container) {
+    container.style.transform = `translateX(-${(pageNumber - 1) * 100}vw)`;
+    currentPage = pageNumber;
+  }
+  updateIcons();
+}
+
+function updateIcons() {
+  const icons = document.querySelectorAll(".icon");
+  icons.forEach((icon, index) => {
+    if (index === currentPage - 1) {
+      icon.classList.add("active");
+    } else {
+      icon.classList.remove("active");
+    }
+  });
+}
+
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
+
+let xDown: number | null = null;
+
+function handleTouchStart(evt: TouchEvent) {
+  const firstTouch = evt.touches[0];
+  xDown = firstTouch.clientX;
+}
+
+function handleTouchMove(evt: TouchEvent) {
+  if (!xDown) {
+    return;
+  }
+
+  let xUp = evt.touches[0].clientX;
+  let xDiff = xDown - xUp;
+
+  if (xDiff > 0) {
+    if (currentPage < 4) {
+      //to increment for every page added "Phorum - Poll - Notifications - IM"
+      showPage(currentPage + 1);
+    }
+  } else {
+    if (currentPage > 1) {
+      showPage(currentPage - 1);
+    }
+  }
+  xDown = null;
+}
+
+////////////////////////////////////////////////////////Poll of the week
