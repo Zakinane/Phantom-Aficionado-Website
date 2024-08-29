3838b32b9655c10b9d7a8be43c7c9cec994da504
Zak <djabazaki@gmail.com>
2024-08-29 21:44:51 +0100

    rearrenging the css files and small tweeks

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var video = document.getElementById("transition");
-setTimeout(function () {
-    if(window.innerWidth <= 768)
-    window.location.replace("html/main.html");
-}, 3000); 
+// var video = document.getElementById("transition");
+// setTimeout(function () {
+//     if(window.innerWidth <= 768)
+//     window.location.replace("html/main.html");
+// }, 3000); 
+
+
+//Useless for now
