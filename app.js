9e3f148dcd7f0e98a8567cb43c2d5cda7e84952b
Zak <djabazaki@gmail.com>
2024-08-31 03:40:01 +0100

    Still trying to make autentification work. help.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 setTimeout(function () {
     if(window.innerWidth <= 768)
-    window.location.replace("html/main.html");
+    window.location.replace("html/main.html"); //later redirect to html/login.html
 }, 3000); 
---
+++ ts/authentication.ts
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDK6GK3Bf1ylbrQwGGoFHd_E_pzqeAls38",
+  authDomain: "phantom-aficionado-website.firebaseapp.com",
+  projectId: "phantom-aficionado-website",
+  storageBucket: "phantom-aficionado-website.appspot.com",
+  messagingSenderId: "47737352678",
+  appId: "1:47737352678:web:78ac293431a3cdafc7a627",
+  measurementId: "G-JT9WWJMPRQ"
+};
+
+// Initialize Firebase
+
+//////////////////////////////////////////////////////LATER
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+
+
+// firebase.auth().signInWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
+//     console.log('Utilisateur connecté:', user);
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     console.error('Erreur d\'authentification:', errorCode, errorMessage);
+//   });
