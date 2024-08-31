00a3673c2b96ed861993f15901359f16e5fd56da
Zak <djabazaki@gmail.com>
2024-08-31 02:00:56 +0100

    gitignore firebase stuff

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// var video = document.getElementById("transition");
-// setTimeout(function () {
-//     if(window.innerWidth <= 768)
-//     window.location.replace("html/main.html");
-// }, 3000); 
-
-
-//Useless for now
+setTimeout(function () {
+    if(window.innerWidth <= 768)
+    window.location.replace("html/main.html");
+}, 3000); 
