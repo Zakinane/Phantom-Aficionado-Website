916780384f17c888eacbbc2aa910aac5e989aaae
Zak <djabazaki@gmail.com>
2024-09-07 21:37:24 +0100

    deleted random images

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 setTimeout(function () {
     if(window.innerWidth <= 768)
-    window.location.replace("html/signup.html"); //later redirect to html/login.html
+    window.location.replace("html/signup.html");
 }, 3000); 
