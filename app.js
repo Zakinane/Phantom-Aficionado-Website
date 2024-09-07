9dfe19d40f06178c2b7e367fc5e175c8c0cca105
Zak <djabazaki@gmail.com>
2024-09-07 16:56:25 +0100

    bug mobile corrected

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 setTimeout(function () {
     if(window.innerWidth <= 768)
-    window.location.replace("html/authentication.html"); //later redirect to html/login.html
+    window.location.replace("./html/signup.html"); //later redirect to html/login.html
 }, 3000); 
