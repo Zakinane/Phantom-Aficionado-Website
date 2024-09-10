da0d803618a7eac403490feb9f333e05d16d9881
Zak <djabazaki@gmail.com>
2024-09-11 00:00:09 +0100

    added the topic window (not functional)

--- ts/addTopics.ts
+++ ts/addTopics.ts
@@ -1,3 +1,15 @@
 let addTopicBtn : HTMLElement|null = document.getElementById("add-btn-phorum");
+let closeTopicPageBtn : HTMLElement|null = document.getElementById("close-topic-btn");
+let addTopicPage : HTMLElement|null = document.getElementById("add-topic-page")
 
-window.location.href = "../html/addTopic.html";
+function closeAddTopicPage() {
+    if (addTopicPage) {
+        addTopicPage.style.display = "none";
+    }
+}
+
+function openAddTopicPage() {
+    if (addTopicPage){
+        addTopicPage.style.display = "flex";
+    }
+ }
